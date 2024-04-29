24290e678284a8d70badcc56077f4027defed93c
Martin Kröger <mk@mat.ethz.ch>
2024-04-29 22:43:51 +0200

    Update install.pl

--- install.pl
+++ install.pl
@@ -1,8 +1,9 @@
 #! /usr/bin/perl 
 
-print "cloning https://github.com/agrawalsamarth/critical_bonds ..\n"; 
-print `git clone https://github.com/agrawalsamarth/critical_bonds`; 
-print `cd critical_bonds`; 
+#print "cloning https://github.com/agrawalsamarth/critical_bonds ..\n"; 
+#print `git clone https://github.com/agrawalsamarth/critical_bonds`; 
+#print `cd critical_bonds`; 
+
 print "cloning https://gitlab.com/libeigen/eigen.git ..\n"; 
 print `git clone https://gitlab.com/libeigen/eigen.git`;
 print "make all (please stand by) ..\n"; 
